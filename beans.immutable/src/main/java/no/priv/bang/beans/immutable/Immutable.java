77de8b5900294539a54490c97e635332422341b2
Steinar Bang <sb@dod.no>
2019-12-28 22:31:58 +0100

    Add immutable bean base class implementing equals() and hashCode() for immutable beans

---
+++ beans.immutable/src/main/java/no/priv/bang/beans/immutable/Immutable.java
@@ -0,0 +1,65 @@
+package no.priv.bang.beans.immutable;
+/*
+ * Copyright 2019 Steinar Bang
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+/**
+ * This class reimplements {@link #hashCode()} and {@link #equals(Object)} for
+ * immutable beans. The immutable bean needing a hashCode() and equals() method
+ * should extend this class.
+ */
+public class Immutable {
+    int _cachedHash = 0; // NOSONAR This field has a non-standard name on purpose to not conflict with any actual field 
+
+    /**
+     * Uses {@link HashCodeBuilder#reflectionHashCode(Object, String...)} to
+     * create a hashcode. The hashcode is lazily created the first time
+     * the method is called, and then cached.
+     *
+     * This only works if the bean is immutable. So don't use Immutable as a base
+     * class for beans with setters.
+     */
+    @Override
+    public int hashCode() {
+        if (_cachedHash == 0) {
+            _cachedHash = HashCodeBuilder.reflectionHashCode(this);
+        }
+
+        return _cachedHash;
+    }
+
+    /**
+     * Returns false if the compared object is null, has a different
+     * class or has a different {@link #hashCode()}.
+     *
+     * Returns true if the compared object is the same object as
+     * this object or if the compared object has the same hashCode().
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || obj.getClass() != getClass()) {
+            return false;
+        }
+
+        if (obj == this) {
+            return true;
+        }
+
+        return hashCode() == obj.hashCode();
+    }
+
+}
---
+++ beans.immutable/src/test/java/no/priv/bang/beans/immutable/ImmutableTest.java
@@ -0,0 +1,80 @@
+package no.priv.bang.beans.immutable;
+/*
+ * Copyright 2019 Steinar Bang
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ImmutableTest {
+    class IntBean extends Immutable {
+        private int field1;
+        private int field2;
+
+        public IntBean(int field1, int field2) {
+            super();
+            this.field1 = field1;
+            this.field2 = field2;
+        }
+
+        public int getField1() {
+            return field1;
+        }
+
+        public int getField2() {
+            return field2;
+        }
+    }
+
+    @Test
+    void testIntBeanIdenticalFields() {
+        IntBean bean1 = new IntBean(1, 42);
+        IntBean bean2 = new IntBean(1, 42);
+        assertEquals(bean1.hashCode(), bean2.hashCode());
+        assertEquals(bean1, bean2);
+        assertNotEquals(0, bean1.hashCode());
+    }
+
+    @Test
+    void testIntBeanNull() {
+        IntBean bean1 = new IntBean(1, 42);
+        IntBean nullBean = null;
+        assertNotEquals(bean1, nullBean);
+    }
+
+    @Test
+    void testIntBeanDifferentClass() {
+        IntBean bean1 = new IntBean(1, 42);
+        String notABean = "not a bean";
+        assertNotEquals(bean1, notABean);
+    }
+
+    @Test
+    void testIntBeanSameBean() {
+        IntBean bean1 = new IntBean(1, 42);
+        assertEquals(bean1, bean1);
+    }
+
+    @Test
+    void testIntBeanIFieldsNotdentical() {
+        IntBean bean1 = new IntBean(1, 42);
+        IntBean bean2 = new IntBean(0, 43);
+        assertNotEquals(bean1.hashCode(), bean2.hashCode());
+        assertNotEquals(bean1, bean2);
+        assertNotEquals(0, bean2.hashCode());
+    }
+
+}
