33b02cad5a7144a79beb4484fb82c470a3c97dd9
Steinar Bang <sb@dod.no>
2020-01-04 13:24:17 +0100

    Implement initial business logic for the fildele application

---
+++ fildele.definitions/src/main/java/no/priv/bang/fildele/definitions/Directory.java
@@ -0,0 +1,7 @@
+package no.priv.bang.fildele.definitions;
+
+import no.priv.bang.beans.immutable.Immutable;
+
+public class Directory extends Immutable { // NOSONAR Immutable provides a satisfactory equals() method
+
+}
