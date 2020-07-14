d39ccc8714fe669ddfe0ca3caf738deeb239aa6d
Steinar Bang <sb@dod.no>
2020-07-14 09:30:11 +0200

    Add liqubase JDBC database schema
    Had to change column name from "path" to "localpath" in table
    albumentries in liquibase, to avoid getting JDBC errors about column
    not found on insert.

---
+++ oldalbum.db.liquibase/src/main/java/no/priv/bang/oldalbum/db/liquibase/OldAlbumLiquibase.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2020 Steinar Bang
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package no.priv.bang.oldalbum.db.liquibase;
+
+import java.sql.Connection;
+
+import liquibase.Liquibase;
+import liquibase.database.DatabaseConnection;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.exception.LiquibaseException;
+import liquibase.resource.ClassLoaderResourceAccessor;
+
+public class OldAlbumLiquibase {
+
+    public void createInitialSchema(Connection connection) throws LiquibaseException {
+        applyLiquibaseChangeLog(connection, "oldalbum/changelog01.xml");
+    }
+
+    private void applyLiquibaseChangeLog(Connection connection, String changelogClasspathResource) throws LiquibaseException {
+        Liquibase liquibase = createLiquibaseInstance(connection, changelogClasspathResource);
+        liquibase.update("");
+    }
+
+    private Liquibase createLiquibaseInstance(Connection connection, String changelogClasspathResource) throws LiquibaseException {
+        DatabaseConnection databaseConnection = new JdbcConnection(connection);
+        ClassLoaderResourceAccessor classLoaderResourceAccessor = new ClassLoaderResourceAccessor(getClass().getClassLoader());
+        return new Liquibase(changelogClasspathResource, classLoaderResourceAccessor, databaseConnection);
+    }
+
+}
