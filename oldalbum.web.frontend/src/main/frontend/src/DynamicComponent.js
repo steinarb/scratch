bce106864f24c433fc3075bb0172ca9c5fdae451
Steinar Bang <sb@dod.no>
2020-07-28 16:58:59 +0200

    Make DynamicComponent a functional component

--- oldalbum.web.frontend/src/main/frontend/src/DynamicComponent.js
+++ oldalbum.web.frontend/src/main/frontend/src/DynamicComponent.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-class DynamicComponent extends Component {
-   render() {
-       const {title, content} = this.props;
-       return (
-           <div>
-               <h1>{title}</h1>
-               <p>{content}</p>
-           </div>
-       );
-   }
+function DynamicComponent(props) {
+    const {title, content} = props;
+    return (
+        <div>
+            <h1>{title}</h1>
+            <p>{content}</p>
+        </div>
+    );
 }
 export default DynamicComponent;
