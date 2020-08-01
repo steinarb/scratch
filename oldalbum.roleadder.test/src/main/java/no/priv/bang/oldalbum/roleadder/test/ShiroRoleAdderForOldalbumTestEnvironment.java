b432c489421d3b06e0acf5070464e9edfcc1a60d
Steinar Bang <sb@dod.no>
2020-08-01 17:49:05 +0200

    Add a DS component that adds shiro role "oldalbumadmin" if the role doesn't already exist

---
+++ oldalbum.roleadder.test/src/main/java/no/priv/bang/oldalbum/roleadder/test/ShiroRoleAdderForOldalbumTestEnvironment.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2020 Steinar Bang
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package no.priv.bang.oldalbum.roleadder.test;
+
+import java.util.Optional;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+import no.priv.bang.osgiservice.users.Role;
+import no.priv.bang.osgiservice.users.UserManagementService;
+
+@Component(immediate = true)
+public class ShiroRoleAdderForOldalbumTestEnvironment {
+
+    private UserManagementService useradmin;
+
+    @Reference
+    public void addUseradmin(UserManagementService useradmin) {
+        this.useradmin = useradmin;
+    }
+
+    @Activate
+    public void activate() {
+        addOldalbumadminRole();
+    }
+
+    public Role addOldalbumadminRole() {
+        Optional<Role> existingrole = useradmin.getRoles().stream().filter(r -> "oldalbumadmin".equals(r.getRolename())).findFirst();
+        if (existingrole.isPresent()) {
+            return existingrole.get();
+        }
+
+        Role role = new Role(0, "oldalbumadmin", "Created by oldalbum.roleadder.test");
+        useradmin.addRole(role);
+        return role;
+    }
+
+}
---
+++ oldalbum.roleadder.test/src/test/java/no/priv/bang/oldalbum/roleadder/test/ShiroRoleAdderForOldalbumTestEnvironmentTest.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2020 Steinar Bang
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package no.priv.bang.oldalbum.roleadder.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+
+import no.priv.bang.osgiservice.users.Role;
+import no.priv.bang.osgiservice.users.UserManagementService;
+
+class ShiroRoleAdderForOldalbumTestEnvironmentTest {
+
+    @Test
+    void testActivate() {
+        UserManagementService useradmin = mock(UserManagementService.class);
+        ShiroRoleAdderForOldalbumTestEnvironment roleadder = new ShiroRoleAdderForOldalbumTestEnvironment();
+        roleadder.addUseradmin(useradmin);
+        roleadder.activate();
+        verify(useradmin, times(1)).getRoles();
+    }
+
+    @Test
+    void testAddOldalbumRoleWhenRoleDoesntExist() {
+        UserManagementService useradmin = mock(UserManagementService.class);
+        ShiroRoleAdderForOldalbumTestEnvironment roleadder = new ShiroRoleAdderForOldalbumTestEnvironment();
+        roleadder.addUseradmin(useradmin);
+        Role role = roleadder.addOldalbumadminRole();
+        assertEquals("oldalbumadmin", role.getRolename());
+        assertEquals("Created by oldalbum.roleadder.test", role.getDescription());
+    }
+
+    @Test
+    void testAddOldalbumRoleWhenRoleAlreadyExist() {
+        UserManagementService useradmin = mock(UserManagementService.class);
+        Role existingRole = new Role(0, "oldalbumadmin", "Already exists");
+        when(useradmin.getRoles()).thenReturn(Arrays.asList(existingRole));
+        ShiroRoleAdderForOldalbumTestEnvironment roleadder = new ShiroRoleAdderForOldalbumTestEnvironment();
+        roleadder.addUseradmin(useradmin);
+        Role role = roleadder.addOldalbumadminRole();
+        assertEquals("oldalbumadmin", role.getRolename());
+        assertEquals("Already exists", role.getDescription());
+    }
+
+}
