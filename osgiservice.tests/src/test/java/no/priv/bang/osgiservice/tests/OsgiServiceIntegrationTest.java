d8f603774403db9a60a5e661e7e7b21288f2950f
Steinar Bang <sb@dod.no>
2021-03-25 22:27:14 +0100

    Add pax exam integration test loading the osgi services karaf features

---
+++ osgiservice.tests/src/test/java/no/priv/bang/osgiservice/tests/OsgiServiceIntegrationTest.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2021 Steinar Bang
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package no.priv.bang.osgiservice.tests;
+
+import static org.ops4j.pax.exam.CoreOptions.maven;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.features;
+
+import java.util.stream.Stream;
+
+import org.apache.karaf.itests.KarafTestSupport;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.options.MavenArtifactUrlReference;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerClass;
+
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerClass.class)
+public class OsgiServiceIntegrationTest extends KarafTestSupport {
+
+    @Configuration
+    public Option[] config() {
+        final MavenArtifactUrlReference databaseFeatureRepo = maven()
+            .groupId("no.priv.bang.osgiservice")
+            .artifactId("osgiservice.database")
+            .version("LATEST")
+            .type("xml")
+            .classifier("features");
+        final MavenArtifactUrlReference usersFeatureRepo = maven()
+            .groupId("no.priv.bang.osgiservice")
+            .artifactId("osgiservice.users")
+            .version("LATEST")
+            .type("xml")
+            .classifier("features");
+        Option[] options = new Option[] { features(databaseFeatureRepo), features(usersFeatureRepo) };
+        return Stream.of(super.config(), options).flatMap(Stream::of).toArray(Option[]::new);
+    }
+
+    @Test
+    public void testLoadDatabaseFeature() throws Exception { // NOSONAR this test has an assert, just not an assert sonar recognizes
+        installAndAssertFeature("sb-database-osgi-service");
+    }
+
+    @Test
+    public void testLoadUsersFeature() throws Exception { // NOSONAR this test has an assert, just not an assert sonar recognizes
+        installAndAssertFeature("user-management-osgi-service");
+    }
+
+}
