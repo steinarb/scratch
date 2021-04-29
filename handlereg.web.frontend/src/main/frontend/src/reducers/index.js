4747b3aa7850d5ffc7cd5dab772b2f3009f718b4
Steinar Bang <sb@dod.no>
2021-04-29 20:49:11 +0200

    Rename handlereg reducer files to have "Reducer" in their file names

--- handlereg.web.frontend/src/main/frontend/src/reducers/index.js
+++ handlereg.web.frontend/src/main/frontend/src/reducers/index.js
@@ -1,22 +1,22 @@
 import { combineReducers } from 'redux';
 import { connectRouter } from 'connected-react-router';
 import { emptyUser, emptyUserAndPasswords, emptyRole, emptyPermission } from '../constants';
-import login from './login';
-import oversikt from './oversikt';
-import butikker from './butikker';
-import butikk from './butikk';
-import valgtButikk from './valgtButikk';
-import handlinger from './handlinger';
-import nyhandling from './nyhandling';
-import sumbutikk from './sumbutikk';
-import handlingerbutikk from './handlingerbutikk';
-import sistehandel from './sistehandel';
-import sumyear from './sumyear';
-import sumyearmonth from './sumyearmonth';
-import brukernavn from './brukernavnReducer';
-import favoritter from './favoritterReducer';
+import login from './loginReducer';
+import oversikt from './oversiktReducer';
+import butikker from './butikkerReducer';
+import butikk from './butikkReducer';
+import valgtButikk from './valgtButikkReducer';
+import handlinger from './handlingerReducer';
+import nyhandling from './nyhandlingReducer';
+import sumbutikk from './sumbutikkReducer';
+import handlingerbutikk from './handlingerbutikkReducer';
+import sistehandel from './sistehandelReducer';
+import sumyear from './sumyearReducer';
+import sumyearmonth from './sumyearmonthReducer';
+import brukernavn from './brukernavnReducerReducer';
+import favoritter from './favoritterReducerReducer';
 import favorittbutikk from './favorittbutikkReducer';
-import errors from './errors';
+import errors from './errorsReducer';
 
 export default (history) => combineReducers({
     router: connectRouter(history),
