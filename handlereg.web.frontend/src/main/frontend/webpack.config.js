a3ac15d84542b1fa1cc70dca6324370e4f320486
Steinar Bang <sb@dod.no>
2021-05-10 22:21:45 +0200

    Upgrade webpack to 5.36.2 with babel 7.14.0

--- handlereg.web.frontend/src/main/frontend/webpack.config.js
+++ handlereg.web.frontend/src/main/frontend/webpack.config.js
@@ -18,19 +18,19 @@ module.exports = {
         rules: [
             {
                 test: /\.jsx?$/,
-                loader: ['babel-loader?' + JSON.stringify({
+                exclude: /node_modules/,
+                use: ['babel-loader?' + JSON.stringify({
                     cacheDirectory: true,
                     presets: ['@babel/preset-react']
                 }), 'eslint-loader'],
-                exclude: /node_modules/,
             },
             {
                 test: /\.css$/,
-                loader: 'style-loader!css-loader'
+                use: [ { loader: 'style-loader' }, { loader: 'css-loader' } ]
             },
             {
                 test: /\.(eot|svg|ttf|woff|woff2|otf)(\??\#?v=[.0-9]+)?$/,
-                loader: 'file-loader?name=[name].[ext]',
+                use: 'file-loader?name=[name].[ext]',
             },
         ]
     }
