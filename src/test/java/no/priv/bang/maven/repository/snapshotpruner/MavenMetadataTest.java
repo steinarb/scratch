67ba80117a8031f5dd2ec991c08eb50ea3d8aca9
Steinar Bang <sb@dod.no>
2021-06-21 22:08:28 +0200

    Replace junit 4.12 with junit jupiter 5.7.2

--- src/test/java/no/priv/bang/maven/repository/snapshotpruner/MavenMetadataTest.java
+++ src/test/java/no/priv/bang/maven/repository/snapshotpruner/MavenMetadataTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Steinar Bang
+ * Copyright 2017-2021 Steinar Bang
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,7 +15,7 @@
  */
 package no.priv.bang.maven.repository.snapshotpruner;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 import static no.priv.bang.maven.repository.snapshotpruner.MavenProperties.*;
 
 import java.io.File;
@@ -27,7 +27,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import org.jdom2.JDOMException;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class MavenMetadataTest {
 
--- src/test/java/no/priv/bang/maven/repository/snapshotpruner/MavenRepositoryTest.java
+++ src/test/java/no/priv/bang/maven/repository/snapshotpruner/MavenRepositoryTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Steinar Bang
+ * Copyright 2017-2021 Steinar Bang
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,7 +15,7 @@
  */
 package no.priv.bang.maven.repository.snapshotpruner;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 import static no.priv.bang.maven.repository.snapshotpruner.MavenProperties.*;
 
 import java.io.IOException;
@@ -23,7 +23,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import org.jdom2.JDOMException;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class MavenRepositoryTest {
 
