1c0ad539a34fc257b164faee4e9d118e814949d9
Steinar Bang <sb@dod.no>
2022-04-13 19:47:50 +0200

    fixup! Simplify and flatten the redux data model

--- ukelonn.web.frontend/src/main/frontend/sagas/userSaga.js
+++ ukelonn.web.frontend/src/main/frontend/sagas/userSaga.js
@@ -15,18 +15,27 @@ import {
 } from '../actiontypes';
 
 function* selectUser(action) {
+    console.log('selectUser(1)');
     const userid = action.payload;
+    console.log('selectUser(2)');
     if (userid === -1) {
+        console.log('selectUser(3)');
         yield put(CLEAR_USER());
     } else {
+        console.log('selectUser(4)');
         const users = yield select(state => state.users);
+        console.log('selectUser(5)');
         const user = users.find(u => u.userid = userid);
+        console.log('selectUser(6)');
         if (user) {
+            console.log('selectUser(7)');
             const { username } = user;
             if (username) {
+                console.log('selectUser(8)');
                 yield put(MODIFY_USER_USERNAME(username));
                 yield put(REQUEST_ADMIN_STATUS(username));
             }
+            console.log('selectUser(9)');
             yield put(MODIFY_USER_EMAIL(user.email));
             yield put(MODIFY_USER_FIRSTNAME(user.firstname));
             yield put(MODIFY_USER_LASTNAME(user.lastname));
