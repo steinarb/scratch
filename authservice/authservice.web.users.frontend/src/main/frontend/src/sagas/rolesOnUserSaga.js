af997df7a77d618790f5dd2ab473b1b5fbdb3889
Steinar Bang <sb@dod.no>
2022-04-30 16:28:30 +0200

    fixup! Reorganize and flatten the redux store state

--- authservice/authservice.web.users.frontend/src/main/frontend/src/sagas/rolesOnUserSaga.js
+++ authservice/authservice.web.users.frontend/src/main/frontend/src/sagas/rolesOnUserSaga.js
@@ -1,6 +1,6 @@
 import { takeLatest, select, put } from 'redux-saga/effects';
 import {
-    USER_UPDATE,
+    USER_SELECTED,
     USERROLES_RECEIVED,
     ROLES_ON_USER_UPDATE,
     ROLES_NOT_ON_USER_UPDATE,
@@ -8,11 +8,11 @@ import {
 import { emptyRole } from '../constants';
 
 function* findRolesOnUsersAndFindRolesNotOnUsers() {
-    const user = yield select(state => state.user);
+    const username = yield select(state => state.username);
     const allRoles = yield select(state => state.roles);
     const roles = allRoles.filter(r => r.id !== emptyRole.id);
     const userroles = yield select(state => state.userroles);
-    const rolesOnUser = userroles[user.username] || [];
+    const rolesOnUser = userroles[username] || [];
     yield put(ROLES_ON_USER_UPDATE(rolesOnUser));
     const rolesNotOnUser = roles.filter(r => !rolesOnUser.find(rou => rou.id === r.id));
     yield put(ROLES_NOT_ON_USER_UPDATE(rolesNotOnUser));
@@ -20,5 +20,5 @@ function* findRolesOnUsersAndFindRolesNotOnUsers() {
 
 export default function* rolesOnUserSaga() {
     yield takeLatest(USERROLES_RECEIVED, findRolesOnUsersAndFindRolesNotOnUsers);
-    yield takeLatest(USER_UPDATE, findRolesOnUsersAndFindRolesNotOnUsers);
+    yield takeLatest(USER_SELECTED, findRolesOnUsersAndFindRolesNotOnUsers);
 }
