054f97b45ef5574e6ad7d3809e0b4015498f169b
Steinar Bang <sb@dod.no>
2022-09-01 20:21:15 +0200

    Upgrade react and react-dom to 18.2.0

--- handlereg.web.frontend/src/main/frontend/src/index.js
+++ handlereg.web.frontend/src/main/frontend/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -25,11 +25,14 @@ sagaMiddleware.run(rootSaga);
 
 store.dispatch(LOGINTILSTAND_HENT());
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
     <Provider store={store}>
         <App history={history} />
     </Provider>,
-    document.getElementById('root'));
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
