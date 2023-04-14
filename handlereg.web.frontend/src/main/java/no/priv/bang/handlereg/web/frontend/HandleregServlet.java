a8d3a82266807faf2d8326589fe82888349f356e
Steinar Bang <sb@dod.no>
2023-04-14 20:26:11 +0200

    Revert "Add "Cache-Control: no-cache" and "Expires: 0" HTTP headers for index.html to ensure fresh rendering when loading on mobile"
    This reverts commit e5da7ca7a75a7d2edc5d9c8eb043efe0af30137a.

--- handlereg.web.frontend/src/main/java/no/priv/bang/handlereg/web/frontend/HandleregServlet.java
+++ handlereg.web.frontend/src/main/java/no/priv/bang/handlereg/web/frontend/HandleregServlet.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2023 Steinar Bang
+ * Copyright 2019-2021 Steinar Bang
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,22 +16,14 @@
 package no.priv.bang.handlereg.web.frontend;
 
 import javax.servlet.Servlet;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 import static org.osgi.service.http.whiteboard.HttpWhiteboardConstants.*;
-
-import java.io.IOException;
-import java.io.InputStream;
-
 import org.osgi.service.http.whiteboard.propertytypes.HttpWhiteboardContextSelect;
 import org.osgi.service.http.whiteboard.propertytypes.HttpWhiteboardServletName;
 import org.osgi.service.http.whiteboard.propertytypes.HttpWhiteboardServletPattern;
 import org.osgi.service.log.LogService;
-import static javax.servlet.http.HttpServletResponse.*;
 
 import no.priv.bang.servlet.frontend.FrontendServlet;
 
@@ -68,23 +60,5 @@ public class HandleregServlet extends FrontendServlet {
     @Reference
     public void setLogService(LogService logservice) {
         super.setLogService(logservice);
-    }
-
-    @Override
-    protected boolean thisIsAResourceThatShouldBeProcessed(HttpServletRequest request, String pathInfo, String resource, String contentType) {
-        return "index.html".equals(resource);
-    }
-
-    @Override
-    protected void processResource(HttpServletResponse response, HttpServletRequest request, String pathInfo, String resource, String contentType) throws IOException {
-        response.setStatus(SC_OK);
-        response.setContentType(contentType);
-        response.setHeader("Cache-Control", "private, no-store, no-cache, must-revalidate");
-        response.setDateHeader("Expires", 0);
-        try(ServletOutputStream responseBody = response.getOutputStream()) {
-            try(InputStream resourceFromClasspath = getClass().getClassLoader().getResourceAsStream(resource)) {
-                copyStream(resourceFromClasspath, responseBody);
-            }
-        }
     }
 }
