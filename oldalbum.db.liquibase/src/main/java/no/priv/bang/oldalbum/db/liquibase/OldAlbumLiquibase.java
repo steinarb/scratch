70b5b6466d7d9c8831da4ee60f4141d85b3c39c2
Steinar Bang <sb@dod.no>
2023-06-17 00:55:05 +0200

    Replace deprecated Liquibase facade with ScopeRunner using ThreadLocalScopeManager in the oldalbum liquibase runner

--- oldalbum.db.liquibase/src/main/java/no/priv/bang/oldalbum/db/liquibase/OldAlbumLiquibase.java
+++ oldalbum.db.liquibase/src/main/java/no/priv/bang/oldalbum/db/liquibase/OldAlbumLiquibase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020-2022 Steinar Bang
+ * Copyright 2020-2023 Steinar Bang
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,9 +16,16 @@
 package no.priv.bang.oldalbum.db.liquibase;
 
 import java.sql.Connection;
+import java.util.Map;
 
-import liquibase.Liquibase;
-import liquibase.database.DatabaseConnection;
+import liquibase.Scope;
+import liquibase.Scope.ScopedRunner;
+import liquibase.changelog.ChangeLogParameters;
+import liquibase.command.CommandScope;
+import liquibase.command.core.UpdateCommandStep;
+import liquibase.command.core.helpers.DatabaseChangelogCommandStep;
+import liquibase.command.core.helpers.DbUrlConnectionCommandStep;
+import liquibase.database.DatabaseFactory;
 import liquibase.database.jvm.JdbcConnection;
 import liquibase.exception.LiquibaseException;
 import liquibase.resource.ClassLoaderResourceAccessor;
@@ -34,11 +41,16 @@ public class OldAlbumLiquibase {
     }
 
     private void applyLiquibaseChangeLog(Connection connection, String changelogClasspathResource) throws LiquibaseException {
-        DatabaseConnection databaseConnection = new JdbcConnection(connection);
-        try(var classLoaderResourceAccessor = new ClassLoaderResourceAccessor(getClass().getClassLoader())) {
-            try(var liquibase = new Liquibase(changelogClasspathResource, classLoaderResourceAccessor, databaseConnection)) {
-                liquibase.update("");
-            }
+        try(var database = DatabaseFactory.getInstance().findCorrectDatabaseImplementation(new JdbcConnection(connection))) {
+            Map<String, Object> scopeObjects = Map.of(
+                Scope.Attr.database.name(), database,
+                Scope.Attr.resourceAccessor.name(), new ClassLoaderResourceAccessor(getClass().getClassLoader()));
+
+            Scope.child(scopeObjects, (ScopedRunner<?>) () -> new CommandScope("update")
+                        .addArgumentValue(DbUrlConnectionCommandStep.DATABASE_ARG, database)
+                        .addArgumentValue(UpdateCommandStep.CHANGELOG_FILE_ARG, changelogClasspathResource)
+                        .addArgumentValue(DatabaseChangelogCommandStep.CHANGELOG_PARAMETERS, new ChangeLogParameters(database))
+                        .execute());
         } catch (LiquibaseException e) {
             throw e;
         } catch (Exception e) {
