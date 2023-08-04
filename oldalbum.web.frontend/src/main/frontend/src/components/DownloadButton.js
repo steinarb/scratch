aedb41de271115cc6eb62012e3357fd7a487e597
Steinar Bang <sb@dod.no>
2023-08-04 20:42:30 +0200

    fixup! Add download picture link on pictures

--- oldalbum.web.frontend/src/main/frontend/src/components/DownloadButton.js
+++ oldalbum.web.frontend/src/main/frontend/src/components/DownloadButton.js
@@ -3,13 +3,14 @@ import { useSelector } from 'react-redux';
 
 export default function DownloadButton(props) {
     const { className = '', item } = props;
+    const basename = useSelector(state => state.router.basename);
     const text = useSelector(state => state.displayTexts);
     const filename = item.imageUrl.split('/').at(-1);
 
     return (
         <a
             className={className}
-            href={'/oldalbum/api/image/download/' + item.id.toString()}
+            href={basename + '/api/image/download/' + item.id.toString()}
             download={filename}
             target="_blank"
             rel="noopener noreferrer"
