18cf142c08ae6cef6b1fce3f08efb061c08a246f
Steinar Bang <sb@dod.no>
2023-08-13 00:04:43 +0200

    Set the correct name for albums in the download attribute in the <a> element doing the download
    Albums don't have an imageUrl, so use the last element of the path, and add ".zip".

--- oldalbum.web.frontend/src/main/frontend/src/components/DownloadButton.js
+++ oldalbum.web.frontend/src/main/frontend/src/components/DownloadButton.js
@@ -5,7 +5,7 @@ export default function DownloadButton(props) {
     const { className = '', item } = props;
     const basename = useSelector(state => state.router.basename);
     const text = useSelector(state => state.displayTexts);
-    const filename = item.imageUrl.split('/').at(-1);
+    const filename = item.album ? item.path.split('/').at(-2) + '.zip' : item.imageUrl.split('/').at(-1);
     const buttonLabel = item.album ? text.downloadalbum : text.downloadpicture;
 
     return (
