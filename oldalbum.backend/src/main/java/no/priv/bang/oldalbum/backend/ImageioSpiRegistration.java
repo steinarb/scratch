0614250f81f922fd750c11390e93f84ee67a9264
Steinar Bang <sb@dod.no>
2023-09-21 22:39:38 +0200

    fixup! Use twelwemonkeys to extract JPEG comment when importing image

--- oldalbum.backend/src/main/java/no/priv/bang/oldalbum/backend/ImageioSpiRegistration.java
+++ oldalbum.backend/src/main/java/no/priv/bang/oldalbum/backend/ImageioSpiRegistration.java
@@ -1,6 +1,7 @@
 package no.priv.bang.oldalbum.backend;
 
 import javax.imageio.spi.IIORegistry;
+import javax.imageio.spi.ImageInputStreamSpi;
 import javax.imageio.spi.ImageReaderSpi;
 import javax.imageio.spi.ImageWriterSpi;
 import javax.imageio.stream.ImageInputStream;
@@ -23,7 +24,7 @@ public class ImageioSpiRegistration {
     }
 
     @Reference(cardinality = ReferenceCardinality.MULTIPLE)
-    public void registerImageInputStreamSpi(ImageInputStream inputStreamProvider) {
+    public void registerImageInputStreamSpi(ImageInputStreamSpi inputStreamProvider) {
         IIORegistry.getDefaultInstance().registerServiceProvider(inputStreamProvider);
     }
 
