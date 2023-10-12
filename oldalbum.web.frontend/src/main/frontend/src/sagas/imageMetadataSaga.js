747613e03bea116554e97734c715e278c5d08073
Steinar Bang <sb@dod.no>
2023-10-12 20:59:58 +0200

    Extract basename from the image URL when adding a picture

--- oldalbum.web.frontend/src/main/frontend/src/sagas/imageMetadataSaga.js
+++ oldalbum.web.frontend/src/main/frontend/src/sagas/imageMetadataSaga.js
@@ -4,6 +4,7 @@ import {
     IMAGE_METADATA_REQUEST,
     IMAGE_METADATA_RECEIVE,
     IMAGE_METADATA_FAILURE,
+    ADD_PICTURE_BASENAME_FIELD_CHANGED,
 } from '../reduxactions';
 
 function doDownloadImageMetadata(url) {
@@ -21,6 +22,16 @@ function* downloadImageMetadata(action) {
     }
 }
 
+function* extractBasename(action) {
+    const url = action.payload;
+    const paths = url.split('/');
+    const filename = paths.pop();
+    const fileAndExtension = filename.split('.');
+    const basename = fileAndExtension.shift();
+    yield put(ADD_PICTURE_BASENAME_FIELD_CHANGED(basename));
+}
+
 export default function* imageMetadataSaga() {
     yield takeLatest(IMAGE_METADATA_REQUEST,  downloadImageMetadata);
+    yield takeLatest(IMAGE_METADATA_REQUEST,  extractBasename);
 }
