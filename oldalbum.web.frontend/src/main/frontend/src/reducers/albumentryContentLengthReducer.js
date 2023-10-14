c7b1ea71ec2ae6d87f139229a20ad5d1f3649a08
Steinar Bang <sb@dod.no>
2023-10-14 19:58:15 +0200

    Replace null metadata values with empty string og 0 in reducers

--- oldalbum.web.frontend/src/main/frontend/src/reducers/albumentryContentLengthReducer.js
+++ oldalbum.web.frontend/src/main/frontend/src/reducers/albumentryContentLengthReducer.js
@@ -10,7 +10,7 @@ const initialState = '';
 const albumentryContentLengthReducer = createReducer(initialState, {
     [FILL_MODIFY_PICTURE_FORM]: (state, action) => action.payload.contentLength,
     [FILL_ADD_PICTURE_FORM]: (state, action) => action.payload.contentLength,
-    [IMAGE_METADATA_RECEIVE]: (state, action) => action.payload.contentLength,
+    [IMAGE_METADATA_RECEIVE]: (state, action) => action.payload.contentLength || 0,
     [CLEAR_PICTURE_FORM]: () => initialState,
 });
 
