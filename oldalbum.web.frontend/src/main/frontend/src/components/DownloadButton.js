7872b99664169a92b54d72ec0ffa6275c618f3f6
Steinar Bang <sb@dod.no>
2023-11-02 20:00:33 +0100

    Handle empty basename correctly in the download button

--- oldalbum.web.frontend/src/main/frontend/src/components/DownloadButton.js
+++ oldalbum.web.frontend/src/main/frontend/src/components/DownloadButton.js
@@ -3,10 +3,11 @@ import { useSelector } from 'react-redux';
 
 export default function DownloadButton(props) {
     const { className = '', item } = props;
-    const basename = useSelector(state => state.router.basename);
+    const routerBasename = useSelector(state => state.router.basename);
     const text = useSelector(state => state.displayTexts);
     const filename = item.album ? item.path.split('/').at(-2) + '.zip' : item.imageUrl.split('/').at(-1);
     const buttonLabel = item.album ? text.downloadalbum : text.downloadpicture;
+    const basename = routerBasename == '/' ? '' : routerBasename;
 
     return (
         <a
