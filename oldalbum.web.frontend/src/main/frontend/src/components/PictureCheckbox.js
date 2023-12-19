87991de61c2ee5515f476bb09106b029ab50763d
Steinar Bang <sb@dod.no>
2023-12-19 23:49:32 +0100

    fixup! Make it possible to select pictures in an album and download selection as a zip file

--- oldalbum.web.frontend/src/main/frontend/src/components/PictureCheckbox.js
+++ oldalbum.web.frontend/src/main/frontend/src/components/PictureCheckbox.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { SELECT_PICTURE_ALBUMENTRY, UNSELECT_PICTURE_ALBUMENTRY } from '../reduxactions';
 
 export default function PictureCheckbox(props) {
-    const { className='' } = props;
+    const { entry, className='' } = props;
+    const dispatch = useDispatch();
     const completeClassName = className + ' picture-checkbox';
 
     return (
-        <input type="checkbox" className={completeClassName} />
+        <input type="checkbox" className={completeClassName} onChange={e => e.target.checked ? dispatch(SELECT_PICTURE_ALBUMENTRY(entry)) : dispatch(UNSELECT_PICTURE_ALBUMENTRY(entry))}/>
     );
 }
