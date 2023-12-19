5c8785f09ec191435e183677ae52c261b06f8a2a
Steinar Bang <sb@dod.no>
2023-12-19 22:40:54 +0100

    Make it possible to select pictures in an album and download selection as a zip file

--- oldalbum.web.frontend/src/main/frontend/src/components/AlbumEntryOfTypePicture.js
+++ oldalbum.web.frontend/src/main/frontend/src/components/AlbumEntryOfTypePicture.js
@@ -16,13 +16,16 @@ function AlbumEntryOfTypePicture(props) {
     return (
         <div id={anchor} className={className + ' col-sm-12 col-md-4 col-lg-3 col-XL-2 album-entry btn btn-light mx-1 my-1'}>
             <LeftButton item={entry} />
-            <NavLink className=' btn btn-light btn-block left-align-cell' to={entry.path}>
-                <ThumbnailImg entry={entry} />
-                <div className="mx-1 container">
-                    <div className="row">{title}</div>
-                    <div className="row text-nowrap">{metadata}</div>
-                </div>
-            </NavLink>
+            <div className="column">
+                <input type="checkbox" className="picture-checkbox left-align-cell" />
+                <NavLink className=' btn btn-light btn-block left-align-cell' to={entry.path}>
+                    <ThumbnailImg entry={entry} />
+                    <div className="mx-1 container">
+                        <div className="row">{title}</div>
+                        <div className="row text-nowrap">{metadata}</div>
+                    </div>
+                </NavLink>
+            </div>
             <RightButton item={entry} />
             <UpDownButton item={entry} />
         </div>
