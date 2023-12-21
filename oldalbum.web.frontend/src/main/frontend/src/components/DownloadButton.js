59d5b92d6339490dfcf242f60d335590d51d80e3
Steinar Bang <sb@dod.no>
2023-12-21 10:53:19 +0100

    fixup! Make it possible to select pictures in an album and download selection as a zip file

--- oldalbum.web.frontend/src/main/frontend/src/components/DownloadButton.js
+++ oldalbum.web.frontend/src/main/frontend/src/components/DownloadButton.js
@@ -5,9 +5,10 @@ import { useSelector } from 'react-redux';
 export default function DownloadButton(props) {
     const { className = '', item } = props;
     const routerBasename = useSelector(state => state.router.basename);
+    const selectedentries = useSelector(state => state.selectedentries);
     const text = useSelector(state => state.displayTexts);
     const filename = item.album ? item.path.split('/').at(-2) + '.zip' : item.imageUrl.split('/').at(-1);
-    const buttonLabel = item.album ? text.downloadalbum : text.downloadpicture;
+    const buttonLabel = item.album ? selectedentries.length ? text.downloadselection : text.downloadalbum : text.downloadpicture;
     const basename = routerBasename == '/' ? '' : routerBasename;
 
     return (
