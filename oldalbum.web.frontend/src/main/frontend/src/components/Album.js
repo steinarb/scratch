2b6a9592b5b50ecab3c2ebcdd3b76a9ef773f7d1
Steinar Bang <sb@dod.no>
2023-12-27 12:20:50 +0100

    fixup! Make it possible to link to a year in an album, and create a local link that appears when hovering over it

--- oldalbum.web.frontend/src/main/frontend/src/components/Album.js
+++ oldalbum.web.frontend/src/main/frontend/src/components/Album.js
@@ -130,7 +130,7 @@ function renderYear(entry) {
         <div id={year} className="column" key={key}>
             <div className="row">
                 <a className="pl-5 btn" data-toggle="collapse" href={collapseRef} aria-expanded={expanded} aria-controls={collapseId}><h2>{year}</h2></a>
-                <a className="picture-checkbox" href={'#' + year.toString()}><h2>#</h2></a>
+                <a className="album-yearlink" href={'#' + year.toString()}><h2>#</h2></a>
             </div>
             <div className="row collapse multi-collapse show pb-5" id={collapseId}>
                 { children.slice().sort((a,b) => a.sort - b.sort).map(renderChild) }
