ae35bc271a67e7dfc2e6e5cfc8f2dc32329e70fa
Steinar Bang <sb@dod.no>
2023-12-28 08:29:31 +0100

    Bugfix: Make "hide all years" button appear initially, when "show years" haven't been toggled at least once

--- oldalbum.web.frontend/src/main/frontend/src/components/AlbumHideShowYearsButton.js
+++ oldalbum.web.frontend/src/main/frontend/src/components/AlbumHideShowYearsButton.js
@@ -5,7 +5,7 @@ export default function AlbumHideShowYearsButton(props) {
     const { album } = props;
     const { id } = album;
     const text = useSelector(state => state.displayTexts);
-    const albumGroupByYear = useSelector(state => !!state.albumGroupByYear[id]);
+    const albumGroupByYear = useSelector(state => state.albumGroupByYear[id] === undefined ? true : state.albumGroupByYear[id]);
     const ariaControls = useSelector(state => id in state.childentriesByYear ? Object.keys(state.childentriesByYear[id]).map(k => 'collapse' + k.toString()).join(' ') : []);
     const [ expanded, setExpanded ] = useState(true);
     const labelText = expanded ? text.hideAllYears : text.showAllYears;
