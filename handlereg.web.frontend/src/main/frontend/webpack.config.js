3b8c044fae7e970bbe0ee8b1d8c257b96514418e
Steinar Bang <sb@dod.no>
2024-01-23 17:37:18 +0100

    Add tailwindcss 3.4.1 processing with webpack to the handlereg react webapp

--- handlereg.web.frontend/src/main/frontend/webpack.config.js
+++ handlereg.web.frontend/src/main/frontend/webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                use: [ { loader: 'style-loader' }, { loader: 'css-loader' } ]
+                use: [ { loader: 'style-loader' }, { loader: 'css-loader' }, { loader: 'postcss-loader' } ]
             },
         ]
     }
