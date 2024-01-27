a21000e9c8e3de6390dc5ee4b4a53541d22cbf27
Steinar Bang <sb@dod.no>
2024-01-27 13:47:13 +0100

    Increase width of AlbumEntryOfTypeAlbum with 3 previews to avoid previews breaking the right edge of the entry

--- oldalbum.web.frontend/src/main/frontend/src/components/AlbumEntryOfTypeAlbum.js
+++ oldalbum.web.frontend/src/main/frontend/src/components/AlbumEntryOfTypeAlbum.js
@@ -17,7 +17,7 @@ export default function AlbumEntryOfTypeAlbum(props) {
     const childrenWithThumbnails = findChildrenThumbnails(entry, children, childentries).slice(0, noOfThumbnails);
     const widthInCols =  noOfThumbnails===2 ?
           ' col-sm-12 col-md-4 col-lg-3 col-xl-2' :
-          ' col-sm-12 col-md-5 col-lg-4 col-xl-3';
+          ' col-sm-12 col-md-7 col-lg-6 col-xl-4 col-xxl-3';
     const anchor = 'entry' + entry.id.toString();
 
     return (
