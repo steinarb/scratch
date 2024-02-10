399f4bad9c7402e5abcecf34a6449629f2f4b9f8
Steinar Bang <sb@dod.no>
2024-02-10 12:06:44 +0100

    Clear modifyFailedError when starting a sort

--- oldalbum.web.frontend/src/main/frontend/src/reducers/modifyFailedErrorReducer.js
+++ oldalbum.web.frontend/src/main/frontend/src/reducers/modifyFailedErrorReducer.js
@@ -2,11 +2,13 @@ import { createReducer } from '@reduxjs/toolkit';
 import {
     SET_MODIFY_FAILED_ERROR,
     CLEAR_ALERT,
+    SORT_ALBUM_ENTRIES_BY_DATE_REQUEST,
 } from '../reduxactions';
 
 const modifyFailedErrorReducer = createReducer('', (builder) => {
     builder
         .addCase(SET_MODIFY_FAILED_ERROR, (state, action) => action.payload)
+        .addCase(SORT_ALBUM_ENTRIES_BY_DATE_REQUEST, () => '')
         .addCase(CLEAR_ALERT, () => '');
 });
 
