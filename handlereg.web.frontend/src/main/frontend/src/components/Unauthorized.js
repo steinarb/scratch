19e985651c878b702641e3cfe675961be6c3c521
Steinar Bang <sb@dod.no>
2024-02-21 19:28:59 +0100

    Style "Unauthorized" react component with tailwind CSS

--- handlereg.web.frontend/src/main/frontend/src/components/Unauthorized.js
+++ handlereg.web.frontend/src/main/frontend/src/components/Unauthorized.js
@@ -12,19 +12,19 @@ export default function Unauthorized() {
 
     return (
         <div>
-            <nav>
-                <a href="../.."><span title="chevron left" aria-hidden="true"></span>&nbsp;Gå hjem!</a>
-                <h1>Ingen tilgang</h1>
-                <div></div>
+            <nav className="flex items-center justify-between flex-wrap bg-slate-100 p-6">
+                <a className="text-center block border border-blue-500 rounded py-2 bg-blue-500 hover:bg-blue-700 text-white" href="../.."><span title="chevron left" aria-hidden="true"></span>&nbsp;Gå hjem!</a>
+                <h1 className="text-3xl font-bold">Ingen tilgang</h1>
+                <div>&nbsp;</div>
             </nav>
             <Container>
                 <p>Hei {loginresultat.brukernavn}! Du har ikke tilgang til denne applikasjonen</p>
                 <p>Klikk &quot;Gå hjem&quot; for å navigere ut av applikasjonen, eller logg ut for å logge inn med en bruker som har tilgang</p>
-                <form onSubmit={ e => { e.preventDefault(); }}>
+                <form className="w-full max-w-lg mt-4 grid grid-flow-row auto-rows-max" onSubmit={ e => { e.preventDefault(); }}>
                     <div>
                         <div/>
                         <div>
-                            <button onClick={() => dispatch(LOGOUT_HENT())}>Logg ut</button>
+                            <button className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => dispatch(LOGOUT_HENT())}>Logg ut</button>
                         </div>
                     </div>
                 </form>
