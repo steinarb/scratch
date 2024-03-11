53f1b61c714abc090c67c7a857879c2a8cd407c6
Steinar Bang <sb@dod.no>
2024-03-11 20:39:29 +0100

    fixup! Get protected paths from the OldAlbum backend and put them before the paths of the static shiro.ini file

--- oldalbum.web.security/src/test/java/no/priv/bang/oldalbum/web/security/OldAlbumShiroFilterTest.java
+++ oldalbum.web.security/src/test/java/no/priv/bang/oldalbum/web/security/OldAlbumShiroFilterTest.java
@@ -18,6 +18,11 @@ package no.priv.bang.oldalbum.web.security;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import java.io.IOException;
+import java.util.LinkedHashMap;
+
+import javax.servlet.ServletException;
+
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.config.Ini;
 import org.apache.shiro.mgt.RealmSecurityManager;
@@ -27,24 +32,51 @@ import org.apache.shiro.session.mgt.eis.MemorySessionDAO;
 import org.apache.shiro.web.env.IniWebEnvironment;
 import org.junit.jupiter.api.Test;
 
+import com.mockrunner.mock.web.MockFilterChain;
+import com.mockrunner.mock.web.MockHttpServletRequest;
+import com.mockrunner.mock.web.MockHttpServletResponse;
+import com.mockrunner.mock.web.MockHttpSession;
+
 import no.priv.bang.oldalbum.services.OldAlbumService;
 
 class OldAlbumShiroFilterTest {
 
     @Test
-    void testAuthenticate() {
+    void testAuthenticate() throws Exception {
         var filter = new OldAlbumShiroFilter();
         var realm = getRealmFromIniFile();
         filter.setRealm(realm);
         var session = new MemorySessionDAO();
         filter.setSession(session);
         var oldalbum = mock(OldAlbumService.class);
+        var protectedUrls = new LinkedHashMap<String, String>();
+        protectedUrls.put("/slides/berglia", "anon");
+        protectedUrls.put("/slides/", "authc");
+        when(oldalbum.findShiroProtectedUrls()).thenReturn(protectedUrls);
         filter.setOldAlbumService(oldalbum);
         filter.activate();
+
         var securitymanager = filter.getSecurityManager();
         var token = new UsernamePasswordToken("jad", "1ad".toCharArray());
         var info = securitymanager.authenticate(token);
         assertEquals(1, info.getPrincipals().asList().size());
+
+        var slidesRequest = buildGetRequest("/slides/");
+        var slidesReqponse = new MockHttpServletResponse();
+        var filterChain = new MockFilterChain();
+        filter.doFilter(slidesRequest, slidesReqponse, filterChain);
+        assertEquals(200, slidesReqponse.getStatusCode());
+    }
+
+    MockHttpServletRequest buildGetRequest(String resource) {
+        var session = new MockHttpSession();
+        return new MockHttpServletRequest()
+            .setProtocol("HTTP/1.1")
+            .setRequestURL("http://localhost:8181/oldalbum" + resource)
+            .setRequestURI("/oldalbum" + resource)
+            .setContextPath("/oldalbum")
+            .setServletPath("/")
+            .setSession(session);
     }
 
     private static Realm getRealmFromIniFile() {
