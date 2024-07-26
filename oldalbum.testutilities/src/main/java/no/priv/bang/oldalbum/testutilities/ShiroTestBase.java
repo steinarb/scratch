8ca6347c994184a1a4c0da93d870d8fa70070886
Steinar Bang <sb@dod.no>
2024-07-26 20:59:45 +0200

    Move ShiroTestBase from oldalbum.web.api to oldalbum.testutilities

--- oldalbum.testutilities/src/main/java/no/priv/bang/oldalbum/testutilities/ShiroTestBase.java
+++ oldalbum.testutilities/src/main/java/no/priv/bang/oldalbum/testutilities/ShiroTestBase.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and limitations
  * under the License.
  */
-package no.priv.bang.oldalbum.web.api;
+package no.priv.bang.oldalbum.testutilities;
 
 import static org.apache.shiro.web.util.WebUtils.SAVED_REQUEST_KEY;
 import static org.mockito.Mockito.mock;
