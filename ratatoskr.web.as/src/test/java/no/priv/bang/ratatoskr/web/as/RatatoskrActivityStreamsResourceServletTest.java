d3bb13642e4f8879685a2811ee70d91fbf3cb94c
Steinar Bang <sb@dod.no>
2026-09-05 12:20:51 +0200

    Parse timestamps in returned JSON in servlet tests as being in system time zone instead of UTC

--- ratatoskr.web.as/src/test/java/no/priv/bang/ratatoskr/web/as/RatatoskrActivityStreamsResourceServletTest.java
+++ ratatoskr.web.as/src/test/java/no/priv/bang/ratatoskr/web/as/RatatoskrActivityStreamsResourceServletTest.java
@@ -23,6 +23,7 @@ import static org.mockito.Mockito.when;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Properties;
+import java.util.TimeZone;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
@@ -38,6 +39,7 @@ import org.osgi.service.log.LogService;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.mockrunner.mock.web.MockHttpServletRequest;
 import com.mockrunner.mock.web.MockHttpServletResponse;
 import com.mockrunner.mock.web.MockHttpSession;
@@ -53,13 +55,14 @@ import no.priv.bang.ratatoskr.services.activitypub.ActivityCollection;
 import no.priv.bang.ratatoskr.services.activitypub.Like;
 import no.priv.bang.ratatoskr.services.activitypub.Person;
 import no.priv.bang.ratatoskr.services.activitypub.PersonCollection;
-import no.priv.bang.ratatoskr.services.activitypub.Status;
 
 class RatatoskrActivityStreamsResourceServletTest extends ShiroTestBase {
 
     public static final ObjectMapper mapper = new ObjectMapper()
+        .findAndRegisterModules()
+        .configure(DeserializationFeature.ADJUST_DATES_TO_CONTEXT_TIME_ZONE, true)
         .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-        .findAndRegisterModules();
+        .setTimeZone(TimeZone.getDefault());
 
     private static DataSource datasource;
 
@@ -245,6 +248,7 @@ class RatatoskrActivityStreamsResourceServletTest extends ShiroTestBase {
 
         servlet.service(request, response);
         assertEquals(200, response.getStatus());
+        System.out.println(response.getOutputStreamContent());
         var liked = mapper.readValue(response.getOutputStreamBinaryContent(), ActivityCollection.class);
         assertThat(liked.id()).isEqualTo("http://localhost:8181/ratatoskr/as/liked/johnd");
         assertThat(liked.totalItems()).isEqualTo(1);
